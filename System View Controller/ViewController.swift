787c1a01f5f88688cd2869efe83a7c99e2c54822
Ivan Nikitin <qqaqq@ya.ru>
2019-04-21 12:50:16 +0700

    Initial Commit

---
+++ System View Controller/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  System View Controller
+//
+//  Created by Ivan Nikitin on 21/04/2019.
+//  Copyright © 2019 Ivan Nikitin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
